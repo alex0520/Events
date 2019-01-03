7a209604d8e45d8237a88c45bc7192caf92e2d25
Alexander Lozano Forero <alexander.lozano@globant.com>
2019-01-03 14:24:42 -0500

    Initial commit with base project's structure

---
+++ src/main/java/com/alozano/partnerships/interview/InterviewApplication.java
@@ -0,0 +1,24 @@
+package com.alozano.partnerships.interview;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class InterviewApplication implements CommandLineRunner {
+	private static final Logger LOGGER = LoggerFactory.getLogger(InterviewApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+	}
+}
---
+++ src/main/java/com/alozano/partnerships/interview/Venue.java
@@ -0,0 +1,8 @@
+package com.alozano.partnerships.interview;
+
+public class Venue {
+    public Integer id;
+    public String name;
+    public String city;
+    public String state;
+}
